f17368aba44f93d02bf537c425cdd0da163d66f7
Lalo104lucky <20223tn021@utez.edu.mx>
2025-10-27 12:51:32 -0600

    [ADD] Back con Category

---
+++ SIGAUT_BACK/src/main/java/com/example/SIGAUT_BACK/models/User.java
@@ -0,0 +1,69 @@
+package com.example.SIGAUT_BACK.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder(builderClassName = "Builder", toBuilder = true)
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String paternalName;
+
+    @Column(length = 50, nullable = false)
+    private String maternalName;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 150, nullable = false)
+    private String password;
+
+    @Column(length = 150)
+    private String direction;
+
+    @Column(length = 512)
+    private String image_url;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Category> categories;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Product> products;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Sale> sales;
+
+
+    public User(String name, String paternalName, String maternalName, String email, String username, String password, String direction, String image_url) {
+        this.name = name;
+        this.paternalName = paternalName;
+        this.maternalName = maternalName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.direction = direction;
+        this.image_url = image_url;
+    }
+}
