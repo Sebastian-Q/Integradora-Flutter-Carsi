b6e9c3dc7567c8e2659a1f2777c4a5643c8969fb
Lalo104lucky <20223tn021@utez.edu.mx>
2025-10-27 10:09:31 -0600

    [ADD] Carpetas de Category, Carpetas Core, Algunos widgets creados para Category

--- SIGAUT_FRONT/lib/main.dart
+++ SIGAUT_FRONT/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sigaut_frontend/features/category/view/categories_screen.dart';
+import 'package:sigaut_frontend/features/others/view/splash_screen.dart';
 import 'package:sigaut_frontend/views/screen/employees_screen.dart';
 import 'package:sigaut_frontend/views/screen/login_screen.dart';
 import 'package:sigaut_frontend/views/screen/products_screen.dart';
@@ -11,22 +13,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const routeName = '/home';
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: LoginScreen.routeName,
+      initialRoute: SplashScreen.routeName,
       routes: {
         LoginScreen.routeName: (args) => const LoginScreen(),
         SaleScreen.routeName: (args) => const SaleScreen(),
         ProfileScreen.routeName: (args) => const ProfileScreen(),
         ProductsScreen.routeName: (args) => const ProductsScreen(),
+        CategoriesScreen.routeName: (args) => const CategoriesScreen(),
         EmployeesScreen.routeName: (args) => const EmployeesScreen(),
         ReportSaleScreen.routeName: (args) => const ReportSaleScreen(),
       },
-      home: Container(),
     );
   }
 }
